14d588c0821df1a5193be982db153d3a65b3cc13
James Bouska <jbouska419@gmail.com>
2014-08-20 17:37:00 -0500

    added navbar.less with examples of how to use

---
+++ examples/navbar/navbar_example.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+<html>
+<head>
+<title>Sizzle Menu Example</title>
+<meta http-equiv="content-type" content="text/html;charset=utf-8" />
+<meta name="generator" content="Geany 0.20" />
+<link rel="stylesheet" type="text/css" href="css/load.css">
+<script src="bower_components/sizzle/dist/sizzle.min.js" type="text/javascript"></script>
+<script src="bower_components/respond/dest/respond.min.js" type="text/javascript"></script>
+<script src="bower_components/html5shiv/dist/html5shiv.min.js" type="text/javascript"></script>
+
+
+</head>
+
+<body>
+	<nav class="menu">
+		<div class="enc-navbar-header">
+			<div id="navbar-collapse" class="enc-navbar-collapse">
+				<a class="enc-navbar-show" href="#navbar-collapse">&#9776;</a>
+				<a class="enc-navbar-hide" href="#">&#9776;</a>
+			<ul class="enc-nav">
+				<li><a href="#">Home</a></li>
+				<li><a href="#">About</a></li>
+				<li><a href="#">FAQ</a></li>
+				<li><a href="#">Photos</a></li>
+				<li><a href="#">Contact</a></li>
+			</ul>
+			</div>
+		</div>
+    
+	</nav>
+	
+	
+	
+</body>
+
+</html>
