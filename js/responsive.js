fd7fbe0a4eede5f8bc9bd2b2a1cab9c2e7327fca
antfuentes87 <antfuentes87@gmail.com>
2015-05-22 16:48:10 -0500

    Add responsive.js & added vertical center function

---
+++ js/responsive.js
@@ -0,0 +1,8 @@
+function encVerticalCenter(parentClassPath, childClassPath, childContentClassPath){
+	$(window).on("load resize scroll",function(e){
+    var parentHeight = $(parentClassPath).height();
+		var childContentHeight = $(childContentClassPath).height();
+		$(childClassPath).css("padding-top", parentHeight / 2);
+		$(childContentClassPath).css("margin-top", -childContentHeight / 2);
+	});
+}
