aab103b10bdf743104223433b8ca22dd6bc239ba
antfuentes87 <antfuentes87@gmail.com>
2015-05-22 16:49:25 -0500

    Updated spacing and remove blank line

--- js/responsive.js
+++ js/responsive.js
@@ -1,6 +1,6 @@
 function encVerticalCenter(parentClassPath, childClassPath, childContentClassPath){
 	$(window).on("load resize scroll",function(e){
-    var parentHeight = $(parentClassPath).height();
+		var parentHeight = $(parentClassPath).height();
 		var childContentHeight = $(childContentClassPath).height();
 		$(childClassPath).css("padding-top", parentHeight / 2);
 		$(childContentClassPath).css("margin-top", -childContentHeight / 2);
