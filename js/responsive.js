4d485ba35b67ee23f5a7fc7472c3aa137e1c299f
antfuentes87 <antfuentes87@gmail.com>
2015-05-26 14:08:00 -0500

    Working with margins

--- js/responsive.js
+++ js/responsive.js
@@ -38,3 +38,47 @@ function encSetHeightMinusPadding(outsideElement, paddingElement, insideElement)
 		$(insideElement).css("height", outsideElementHeight - paddingElementValue);
 	});
 }
+
+function encEvenMargin(margin, elementA, elementB, elementC, elementD){
+	$(window).on("load resize scroll",function(e){
+	
+		var marginHalfPx = margin.replace("px", '');
+		var marginHalf = Math.floor(marginHalfPx) / 2;
+
+		$(elementA).css("margin-top", margin);
+		$(elementA).css("margin-bottom", marginHalf);
+		$(elementA).css("margin-left", margin);
+		$(elementA).css("margin-right", marginHalf);
+
+		$(elementB).css("margin-top", margin);
+		$(elementB).css("margin-bottom", marginHalf);
+		$(elementB).css("margin-left", marginHalf);
+		$(elementB).css("margin-right", margin);
+
+		$(elementC).css("margin-top", marginHalf);
+		$(elementC).css("margin-bottom", margin);
+		$(elementC).css("margin-left", margin);
+		$(elementC).css("margin-right", marginHalf);
+
+		$(elementD).css("margin-top", marginHalf);
+		$(elementD).css("margin-bottom", margin);
+		$(elementD).css("margin-left", marginHalf);
+		$(elementD).css("margin-right", margin);
+	});
+}
+
+function encSetSameHeightMinusMargin(elementA, elementB){
+	$(window).on("load resize scroll",function(e){
+		var elementAHeight = $(elementA).height();
+		
+		var elementBMarginLeft = $(elementB).css("margin-left").replace("px", '');
+		var elementBMarginBottom = $(elementB).css("margin-bottom").replace("px", '');
+		
+		var elementBMarginLeftNumber = Math.floor(elementBMarginLeft);
+		var elementBMarginBottomNumber = Math.floor(elementBMarginBottom);
+		
+		var elementBTotalMargin = elementBMarginLeftNumber + elementBMarginBottomNumber;
+
+		$(elementB).css("height", elementAHeight - elementBTotalMargin);
+	});
+}
