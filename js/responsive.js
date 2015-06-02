801013d994ee59d2b5dbb14202d77106ab64d983
Erik Balonier <erikibali@gmail.com>
2015-06-02 18:33:12 -0500

    changed naming

--- js/responsive.js
+++ js/responsive.js
@@ -1,89 +1,108 @@
-function encVerticalCenter(parentClassPath, childClassPath, childContentClassPath){
-	$(window).on("load resize scroll",function(e){
-		var parentHeight = $(parentClassPath).height();
-		var childContentHeight = $(childContentClassPath).height();
-		$(childClassPath).css("padding-top", parentHeight / 2);
-		$(childContentClassPath).css("margin-top", -childContentHeight / 2);
-	});
+function encVerticalCenter(outerElement, innerElement, innerElementContent){
+	var outerElementHeight = $(outerElement).height();
+	var innerElementContentHeight = $(innerElementContent).height();
+	$(innerElement).css("padding-top", outerElementHeight / 2);
+	$(innerElementContent).css("margin-top", -innerElementContentHeight / 2);
 }
 
-function encSetSameHeight(classPathA, classPathB){
-	$(window).on("load resize scroll",function(e){
-		var classHeightB = $(classPathA).height();
-		$(classPathB).css("height", classHeightB);
-	});
+function  encAllHeightsEqualElementA(elementA, elementB){
+	var elementAHeight = $(elementA).height();
+	$(elementB).height(elementAHeight);
 }
 
-function encSetHalfHeight(classPathA, classPathB){
-	$(window).on("load resize scroll",function(e){
-		var classHeightB = $(classPathA).height();
-		$(classPathB).css("height", classHeightB / 2);
-	});
+function  encAllHeightsEqualElementA_Half(elementA, elementB){
+	var elementAHeight = $(elementA).height();
+	$(elementB).height(elementAHeight / 2);
 }
 
-function encParallellHeight(elementA, elementB, elementRatio){
-	$(window).on("load resize scroll",function(e){
-		var elementAHeight = $(elementA).height();
-		var elementBHeight = elementAHeight / elementRatio;
-		var elementBMarginTop = elementAHeight - elementBHeight;
-		$(elementB).css("height", elementBHeight);
-		$(elementB).css("margin-top", elementBMarginTop);
-	});
+function  encAllHeightsEqualElementA_Offset(elementA, elementB, elementOffset){
+	var elementAHeight = $(elementA).height();
+	var elementBHeight = elementAHeight / elementOffset;
+	var elementBMarginTop = elementAHeight - elementBHeight;
+	$(elementB).height(elementBHeight);
+	$(elementB).css("margin-top", elementBMarginTop);
 }
 
-function encSetHeightMinusPadding(outsideElement, paddingElement, insideElement){
-	$(window).on("load resize scroll",function(e){
-		var paddingElementValue = $(paddingElement).css("padding").replace("px", "");
-		var outsideElementHeight = $(outsideElement).height();
-		$(insideElement).css("height", outsideElementHeight - paddingElementValue);
-	});
+function  encAllHeightsEqualElementA_MinusPadding(elementA, elementB){
+	var elementAHeight = $(elementA).height();
+	var elementAPaddingTop = $(elementA).css("padding-top").replace("px", "");
+	var elementAPaddingRight = $(elementA).css("padding-right").replace("px", "");
+	var elementAPaddingBottom = $(elementA).css("padding-bottom").replace("px", "");
+	var elementAPaddingLeft = $(elementA).css("padding-left").replace("px", "");
+	if(elementAPaddingTop != ""){
+		if(elementAPaddingBottom != ""){
+			var elementAPadding = elementAPaddingTop + elementAPaddingBottom;	
+		}else{
+			var elementAPadding = elementAPaddingTop;
+		}
+	}else if(elementAPaddingBottom != ""){
+		if(elementAPaddingTop != ""){
+			var elementAPadding = elementAPaddingBottom + elementAPaddingTop;
+		}else{
+			var elementAPadding = elementAPaddingBottom;
+		}
+	}		
+	}else if(elementAPaddingRight != ""){
+		var elementAPadding = elementAPaddingRight;
+	}else if(elementAPaddingLeft != ""){
+		var elementAPadding = elementAPaddingLeft;
+	}
+	$(elementB).height(elementAHeight - elementAPadding);
 }
 
-function encEvenMargin(margin, elementA, elementB, elementC, elementD){
-	$(window).on("load resize scroll",function(e){
-	
-		var marginHalfPx = margin.replace("px", '');
-		var marginHalf = Math.floor(marginHalfPx) / 2;
+function encEvenMarginFourElements(margin, elementA, elementB, elementC, elementD){
+	var marginHalfPx = margin.replace("px", '');
+	var marginHalf = Math.floor(marginHalfPx) / 2;
 
-		$(elementA).css("margin-top", margin);
-		$(elementA).css("margin-bottom", marginHalf);
-		$(elementA).css("margin-left", margin);
-		$(elementA).css("margin-right", marginHalf);
+	$(elementA).css("margin-top", margin);
+	$(elementA).css("margin-bottom", marginHalf);
+	$(elementA).css("margin-left", margin);
+	$(elementA).css("margin-right", marginHalf);
 
-		$(elementB).css("margin-top", margin);
-		$(elementB).css("margin-bottom", marginHalf);
-		$(elementB).css("margin-left", marginHalf);
-		$(elementB).css("margin-right", margin);
+	$(elementB).css("margin-top", margin);
+	$(elementB).css("margin-bottom", marginHalf);
+	$(elementB).css("margin-left", marginHalf);
+	$(elementB).css("margin-right", margin);
 
-		$(elementC).css("margin-top", marginHalf);
-		$(elementC).css("margin-bottom", margin);
-		$(elementC).css("margin-left", margin);
-		$(elementC).css("margin-right", marginHalf);
+	$(elementC).css("margin-top", marginHalf);
+	$(elementC).css("margin-bottom", margin);
+	$(elementC).css("margin-left", margin);
+	$(elementC).css("margin-right", marginHalf);
 
-		$(elementD).css("margin-top", marginHalf);
-		$(elementD).css("margin-bottom", margin);
-		$(elementD).css("margin-left", marginHalf);
-		$(elementD).css("margin-right", margin);
-	});
+	$(elementD).css("margin-top", marginHalf);
+	$(elementD).css("margin-bottom", margin);
+	$(elementD).css("margin-left", marginHalf);
+	$(elementD).css("margin-right", margin);
 }
 
-function encSetSameHeightMinusMargin(elementA, elementB){
-	$(window).on("load resize scroll",function(e){
-		var elementAHeight = $(elementA).height();
-		
-		var elementBMarginLeft = $(elementB).css("margin-left").replace("px", '');
-		var elementBMarginBottom = $(elementB).css("margin-bottom").replace("px", '');
-		
-		var elementBMarginLeftNumber = Math.floor(elementBMarginLeft);
-		var elementBMarginBottomNumber = Math.floor(elementBMarginBottom);
-		
-		var elementBTotalMargin = elementBMarginLeftNumber + elementBMarginBottomNumber;
-
-		$(elementB).css("height", elementAHeight - elementBTotalMargin);
-	});
+function  encAllHeightsEqualElementA_MinusMargin(elementA, elementB){
+	var elementAHeight = $(elementA).height();
+	var elementAMarginTop = $(elementA).css("margin-top").replace("px", "");
+	var elementAMarginRight = $(elementA).css("margin-right").replace("px", "");
+	var elementAMarginBottom = $(elementA).css("margin-bottom").replace("px", "");
+	var elementAMarginLeft = $(elementA).css("margin-left").replace("px", "");
+	if(elementAMarginTop != ""){
+		if(elementAMarginBottom != ""){
+			var elementAMargin = elementAMarginTop + elementAMarginBottom;	
+		}else{
+			var elementAMargin = elementAMarginTop;
+		}
+	}else if(elementAMarginBottom != ""){
+		if(elementAMarginTop != ""){
+			var elementAMargin = elementAMarginBottom + elementAMarginTop;
+		}else{
+			var elementAMargin = elementAMarginBottom;
+		}
+	}		
+	}else if(elementAMarginRight != ""){
+		var elementAMargin = elementAMarginRight;
+	}else if(elementAMarginLeft != ""){
+		var elementAMargin = elementAMarginLeft;
+	}
+	$(elementB).height(elementAHeight - elementAMargin);
 }
 
-function encFlexGridHeight(container){
+function encAllHeightsEqualElement_Tallest(container){
 	var currentTallest = 0,
 	currentRowStart = 0,
 	rowDivs = new Array(),
