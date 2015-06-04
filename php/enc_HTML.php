c997261faf156b7310deac70345b723f8635e494
antfuentes87 <antfuentes87@gmail.com>
2015-06-03 20:12:33 -0500

    Made a class

---
+++ php/enc_HTML.php
@@ -0,0 +1,44 @@
+<?php
+/*SECTION ARRAY EXAMPLE*/
+/*
+$whatHomeTeaser = array(
+	"id" => "what-home-teaser",
+	"title" => "The Red Carpet",
+	"subTitle" => "Experience",
+	"buttonText" => "Reading",
+	"buttonLink" => "google.com",
+	"figurePage" => "replaceHTML"
+);
+*/
+
+class enc_HTML{
+	public function paragraphs($sectionArray, $array){
+		foreach($array as $arrayKey => $arrayValue){
+			echo '<p class="'.$sectionArray[id].'-p">'.$arrayValue.'</p>';
+		}
+	}
+
+	public function button($array){
+		echo '<a href="'.$array[buttonLink].'" class="'.$array[id].'-button">';
+			echo $array[buttonText];
+		echo '</a>';
+	}
+
+	public function hGroup($array, $titleHeadingNumber, $subTitleHeadingNumber){
+		echo '<hgroup class="'.$array[id].'-hgroup">';
+			echo '<h'.$titleHeadingNumber.'>';
+				echo $array[title];
+			echo '</h'.$titleHeadingNumber.'>';
+			echo '<h'.$subTitleHeadingNumber.'>';		
+				echo $array[subTitle];
+			echo '</h'.$subTitleHeadingNumber.'>';
+		echo '</hgroup>';
+	}
+
+	public function repeat($sectionArray, $arraySize, $page){
+		for ($key = 0; $key <= $arraySize; $key++) {
+			require($sectionArray[$page].'.php');
+		}
+	}
+}
+?>
