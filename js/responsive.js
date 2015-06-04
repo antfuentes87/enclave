d19228e1a6e13ef9a3e98e994b06323789ee6326
antfuentes87 <antfuentes87@gmail.com>
2015-06-04 17:06:03 -0500

    Added line height

--- js/responsive.js
+++ js/responsive.js
@@ -1,6 +1,7 @@
-function encFlexFont(elementA, elementB, ratio){
+function encFlexFont(elementA, elementB, fontRatio, lineheightRatio){
 	var elementAHeight = $(elementA).height();
-	$(elementB).css("font-size", elementAHeight / ratio);
+	$(elementB).css("font-size", elementAHeight / fontRatio);
+	$(elementB).css("line-height", elementAHeight / lineheightRatio);
 }
 
 function encFlexPaddingTopBottom(elementA, elementB){
