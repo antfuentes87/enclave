25b0ec21fdcdb7ab0ae2de3d0805bdc81dbe9fee
Erik Balonier <erikibali@gmail.com>
2015-07-15 18:26:19 -0500

    little change hgroup

--- php/enc_HTML.php
+++ php/enc_HTML.php
@@ -3,15 +3,16 @@
 class enc_HTML{
 	public function hgroup($firstHeading, $secondHeading, $firstNum, $secondNum, $class, $id){
 		if($class <> '' || $id <> ''){
-			if($class <> '' && $id <> ''){
+			if($class <> '' AND $id <> ''){
 				$html = '<hgroup class="'.$class.'" id="'.$id.'">';
-			}
-			if($class <> ''){
-				$html = '<hgroup class="'.$class.'">';
-			}
-			if($id <> ''){
-				$html = '<hgroup id="'.$id.'">';
-			}
+			}else{
+				if($class <> ''){
+					$html = '<hgroup class="'.$class.'">';
+				}
+				if($id <> ''){
+					$html = '<hgroup id="'.$id.'">';
+				}
+			}		
 		}else{
 			$html = '<hgroup>';
 		}
